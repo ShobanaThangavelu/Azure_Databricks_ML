4f09200b89dbdb90538288f185b3509e3f22e8d7
ShobanaThangavelu <157873160+ShobanaThangavelu@users.noreply.github.com>
2025-11-24 21:20:10 -0800

    Clean up documentation in 01_Fraud_MLmodel.py
    Removed markdown documentation and links related to Databricks fraud framework.

--- 01_Fraud_MLmodel.py
+++ 01_Fraud_MLmodel.py
@@ -1,30 +1,3 @@
-# Databricks notebook source
-# MAGIC %md 
-# MAGIC You may find this series of notebooks at https://github.com/databricks-industry-solutions/fraud-orchestration. For more information about this solution accelerator, visit https://www.databricks.com/solutions/accelerators/fraud-detection.
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC <img src=https://brysmiwasb.blob.core.windows.net/demos/dff/databricks_fsi_white.png width="600px">
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC
-# MAGIC # Databricks fraud framework - Model building
-# MAGIC
-# MAGIC The financial service industry (FSI) is rushing towards transformational change to support new channels and services, delivering transactional features and facilitating payments through new digital channels to remain competitive. Unfortunately, the speed and convenience that these capabilities afford is a benefit to consumers and fraudsters alike. Building a fraud framework often goes beyond just creating a highly accurate machine learning model due ever changing landscape and customer expectation. Oftentimes it involves a complex decision science setup which combines rules engine with a need for a robust and scalable machine learning platform. In this series of notebook, we'll be demonstrating how `Delta Lake`, `MLFlow` and a unified analytics platform can help organisations combat fraud more efficiently
-# MAGIC
-# MAGIC
-# MAGIC ---
-# MAGIC + <a href="$./01_dff_model">STAGE1</a>: Integrating rule based with ML
-# MAGIC + <a href="$./02_dff_orchestration">STAGE2</a>: Building a fraud detection model
-# MAGIC ---
-# MAGIC
-# MAGIC + <sri.ghattamaneni@databricks.com>
-# MAGIC + <nikhil.gupta@databricks.com>
-# MAGIC + <ricardo.portilla@databricks.com>
-
 # COMMAND ----------
 
 # DBTITLE 1,Interpretable ML Visualization Library Import
